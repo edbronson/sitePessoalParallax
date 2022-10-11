77e327152e1fb32c319b80fbe38ddacae85771be
Eder <ederbronson@gmail.com>
2022-10-11 14:48:42 -0300

    responsivo avançando

--- script.js
+++ script.js
@@ -38,3 +38,20 @@ document.addEventListener("scroll", () => {
         })
     }
 })
+
+function btnAbreMenu() {
+    menu = document.getElementsByClassName("menu");
+    menu[0].style.display="block";
+
+    btnMenu = document.getElementsByClassName("btn-menu");
+    btnMenu[0].style.display = "none";
+}
+
+
+function btnFechaMenu() {
+    menu = document.getElementsByClassName("menu");
+    menu[0].style.display="none";
+
+    btnMenu = document.getElementsByClassName("btn-menu");
+    btnMenu[0].style.display="block";
+}
